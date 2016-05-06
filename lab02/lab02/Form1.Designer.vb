53fea38ca9fd14beeba844bf569c422a973a6724
Zullybur <mattcasiro@gmail.com>
2016-05-06 12:26:32 -0700

    lab02 complete

---
+++ lab02/lab02/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Pane1 = New System.Windows.Forms.Panel()
+        Me.picBoxDie2 = New System.Windows.Forms.PictureBox()
+        Me.picBoxDie1 = New System.Windows.Forms.PictureBox()
+        Me.btnRoll = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.tbResults = New System.Windows.Forms.TextBox()
+        Me.lblTimerCount = New System.Windows.Forms.Label()
+        Me.lblCash = New System.Windows.Forms.Label()
+        Me.lblPoint = New System.Windows.Forms.Label()
+        Me.lblRollNumber = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblBet = New System.Windows.Forms.Label()
+        Me.tbBetAmount = New System.Windows.Forms.MaskedTextBox()
+        Me.cbNoRolls = New System.Windows.Forms.CheckBox()
+        Me.Pane1.SuspendLayout()
+        CType(Me.picBoxDie2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBoxDie1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Pane1
+        '
+        Me.Pane1.BackColor = System.Drawing.SystemColors.Window
+        Me.Pane1.Controls.Add(Me.picBoxDie2)
+        Me.Pane1.Controls.Add(Me.picBoxDie1)
+        Me.Pane1.Location = New System.Drawing.Point(289, 12)
+        Me.Pane1.Name = "Pane1"
+        Me.Pane1.Size = New System.Drawing.Size(312, 156)
+        Me.Pane1.TabIndex = 0
+        '
+        'picBoxDie2
+        '
+        Me.picBoxDie2.Location = New System.Drawing.Point(159, 3)
+        Me.picBoxDie2.Name = "picBoxDie2"
+        Me.picBoxDie2.Size = New System.Drawing.Size(150, 150)
+        Me.picBoxDie2.TabIndex = 1
+        Me.picBoxDie2.TabStop = False
+        '
+        'picBoxDie1
+        '
+        Me.picBoxDie1.Location = New System.Drawing.Point(3, 3)
+        Me.picBoxDie1.Name = "picBoxDie1"
+        Me.picBoxDie1.Size = New System.Drawing.Size(150, 150)
+        Me.picBoxDie1.TabIndex = 0
+        Me.picBoxDie1.TabStop = False
+        '
+        'btnRoll
+        '
+        Me.btnRoll.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRoll.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRoll.Location = New System.Drawing.Point(289, 208)
+        Me.btnRoll.Name = "btnRoll"
+        Me.btnRoll.Size = New System.Drawing.Size(312, 72)
+        Me.btnRoll.TabIndex = 1
+        Me.btnRoll.Text = "Roll Dice"
+        Me.btnRoll.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnQuit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuit.Location = New System.Drawing.Point(289, 320)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(312, 72)
+        Me.btnQuit.TabIndex = 2
+        Me.btnQuit.Text = "Cash Out"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'tbResults
+        '
+        Me.tbResults.BackColor = System.Drawing.SystemColors.Window
+        Me.tbResults.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbResults.Location = New System.Drawing.Point(12, 435)
+        Me.tbResults.Multiline = True
+        Me.tbResults.Name = "tbResults"
+        Me.tbResults.ReadOnly = True
+        Me.tbResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbResults.Size = New System.Drawing.Size(589, 295)
+        Me.tbResults.TabIndex = 3
+        '
+        'lblTimerCount
+        '
+        Me.lblTimerCount.AutoSize = True
+        Me.lblTimerCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimerCount.Location = New System.Drawing.Point(21, 9)
+        Me.lblTimerCount.Name = "lblTimerCount"
+        Me.lblTimerCount.Size = New System.Drawing.Size(111, 32)
+        Me.lblTimerCount.TabIndex = 4
+        Me.lblTimerCount.Text = "0:00:00"
+        '
+        'lblCash
+        '
+        Me.lblCash.AutoSize = True
+        Me.lblCash.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCash.Location = New System.Drawing.Point(21, 87)
+        Me.lblCash.Name = "lblCash"
+        Me.lblCash.Size = New System.Drawing.Size(81, 32)
+        Me.lblCash.TabIndex = 5
+        Me.lblCash.Text = "Cash"
+        '
+        'lblPoint
+        '
+        Me.lblPoint.AutoSize = True
+        Me.lblPoint.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPoint.Location = New System.Drawing.Point(21, 243)
+        Me.lblPoint.Name = "lblPoint"
+        Me.lblPoint.Size = New System.Drawing.Size(182, 32)
+        Me.lblPoint.TabIndex = 6
+        Me.lblPoint.Text = "Current Point"
+        '
+        'lblRollNumber
+        '
+        Me.lblRollNumber.AutoSize = True
+        Me.lblRollNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRollNumber.Location = New System.Drawing.Point(21, 321)
+        Me.lblRollNumber.Name = "lblRollNumber"
+        Me.lblRollNumber.Size = New System.Drawing.Size(148, 32)
+        Me.lblRollNumber.TabIndex = 7
+        Me.lblRollNumber.Text = "Roll Count"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        '
+        'lblBet
+        '
+        Me.lblBet.AutoSize = True
+        Me.lblBet.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBet.Location = New System.Drawing.Point(21, 146)
+        Me.lblBet.Name = "lblBet"
+        Me.lblBet.Size = New System.Drawing.Size(171, 32)
+        Me.lblBet.TabIndex = 0
+        Me.lblBet.Text = "Bet Amount:"
+        '
+        'tbBetAmount
+        '
+        Me.tbBetAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbBetAmount.Location = New System.Drawing.Point(27, 181)
+        Me.tbBetAmount.Mask = "0000"
+        Me.tbBetAmount.Name = "tbBetAmount"
+        Me.tbBetAmount.Size = New System.Drawing.Size(75, 39)
+        Me.tbBetAmount.TabIndex = 10
+        '
+        'cbNoRolls
+        '
+        Me.cbNoRolls.AutoSize = True
+        Me.cbNoRolls.Location = New System.Drawing.Point(369, 741)
+        Me.cbNoRolls.Name = "cbNoRolls"
+        Me.cbNoRolls.Size = New System.Drawing.Size(232, 24)
+        Me.cbNoRolls.TabIndex = 11
+        Me.cbNoRolls.Text = "De-Activate Roll Animations"
+        Me.cbNoRolls.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(612, 773)
+        Me.Controls.Add(Me.cbNoRolls)
+        Me.Controls.Add(Me.tbBetAmount)
+        Me.Controls.Add(Me.lblBet)
+        Me.Controls.Add(Me.lblRollNumber)
+        Me.Controls.Add(Me.lblPoint)
+        Me.Controls.Add(Me.lblCash)
+        Me.Controls.Add(Me.lblTimerCount)
+        Me.Controls.Add(Me.tbResults)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnRoll)
+        Me.Controls.Add(Me.Pane1)
+        Me.Name = "Form1"
+        Me.Text = "Dice Game"
+        Me.Pane1.ResumeLayout(False)
+        CType(Me.picBoxDie2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBoxDie1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Pane1 As Panel
+    Friend WithEvents picBoxDie2 As PictureBox
+    Friend WithEvents picBoxDie1 As PictureBox
+    Friend WithEvents btnRoll As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents tbResults As TextBox
+    Friend WithEvents lblTimerCount As Label
+    Friend WithEvents lblCash As Label
+    Friend WithEvents lblPoint As Label
+    Friend WithEvents lblRollNumber As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents lblBet As Label
+    Friend WithEvents tbBetAmount As MaskedTextBox
+    Friend WithEvents cbNoRolls As CheckBox
+End Class
---
+++ lab02/lab02/Form1.vb
@@ -0,0 +1,295 @@
+﻿' Quasi Craps - A basic version of a Craps dice game.
+' Created By: Matthew Casiro
+' Created On: April 26 2016
+Public Class Form1
+    ' Class Scope Variable Declarations
+    Private Const NUM_SIDES As Integer = 6
+    Private rndRoll As New Random
+    Private intCash, intBetAmount, intRollCounter, intPoint, intSum As Integer
+    Private blnWonRound, blnLostRound, blnPointSet As Boolean
+    Private blnPrintDebug, blnRollAnimations As Boolean
+    Private datStartTime As Date
+    Private intTimerTicker, intDieOne, intDieTwo As Integer
+    ' Initialize variables on form load
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        intCash = 100
+        lblCash.Text = "Cash: $" & intCash
+        intBetAmount = 10
+        tbBetAmount.Text = intBetAmount
+        lblBet.Text = "Bet Amount:"
+        intPoint = 0
+        intDieOne = 0
+        intDieTwo = 0
+        lblPoint.Text = "Point: " & intPoint
+        lblRollNumber.Text = "Roll #: " & intRollCounter
+        blnLostRound = False
+        blnWonRound = False
+        blnPointSet = False
+        blnRollAnimations = True
+        datStartTime = Date.Now
+
+#If DEBUG Then
+        blnPrintDebug = True
+#End If
+    End Sub
+    ' Return a randomly generated number based on the size of the die in play
+    Private Function rollDie() As Integer
+        Return rndRoll.Next(1, NUM_SIDES + 1)
+    End Function
+    ' Track and display game timer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim timePlayed As TimeSpan = Date.Now.Subtract(datStartTime)
+        lblTimerCount.Text = String.Format("{0}:{1:d2}:{2:d2}",
+                                           timePlayed.Hours,
+                                           timePlayed.Minutes,
+                                           timePlayed.Seconds)
+    End Sub
+    ' Roll dice and handle results according to game rules
+    ' Calls spinDice() for animations
+    ' Calls logRoll() for printout to form
+    ' Calls endRound() on reaching a win or lose condition
+    Private Async Sub btnRoll_Click(sender As Object, e As EventArgs) Handles btnRoll.Click
+        btnRoll.Enabled = False
+        btnQuit.Enabled = False
+        tbBetAmount.Enabled = False
+        Pane1.Select()
+        ' Roll dice and display resut to log
+        intRollCounter += 1
+        lblRollNumber.Text = "Roll #: " & intRollCounter
+        intDieOne = rollDie()
+        intDieTwo = rollDie()
+        If blnRollAnimations Then
+            spinDice()
+            ' Wait for the dice to finish rolling animation
+            Await Task.Delay(5000)
+        Else
+            setDiceImage(intDieOne, intDieTwo)
+        End If
+        intSum = intDieOne + intDieTwo
+        ' Process roll result for fresh round (point not set)
+        If Not blnPointSet Then
+            If intSum = 7 Or intSum = 11 Then
+                logRoll("Win")
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intSum: " & intSum)
+                blnWonRound = True
+                endRound()
+            ElseIf intSum = 2 Or intSum = 3 Or intSum = 12 Then
+                logRoll("Lose")
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intSum: " & intSum)
+                blnLostRound = True
+                endRound()
+            ElseIf 0 < intSum And intSum <= 12 Then
+                blnPointSet = True
+                intPoint = intSum
+                lblPoint.Text = "Point: " & intPoint
+                logRoll("Point")
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intSum: " & intSum)
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intPoint: " & intPoint)
+            Else
+                logRoll("Error")
+            End If
+        Else
+            ' Process roll result for round in progress (point set)
+            If intSum = intPoint Then
+                logRoll("Win")
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intSum: " & intSum)
+                blnWonRound = True
+                endRound()
+            ElseIf intSum = 7 Then
+                logRoll("Lose")
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intSum: " & intSum)
+                blnLostRound = True
+                endRound()
+            Else
+                If blnPrintDebug Then Debug.WriteLine("DEBUG intSum: " & intSum)
+                logRoll("")
+            End If
+        End If
+        ' Unlock custom bet textbox if no point established
+        If Not blnPointSet Then
+            tbBetAmount.Enabled = True
+        End If
+        btnRoll.Enabled = True
+        btnQuit.Enabled = True
+    End Sub
+    ' Verify and update bet amount when user enters custom value
+    Private Sub tbBetAmount_Leave(sender As Object, e As EventArgs) Handles tbBetAmount.Leave
+        ' Check for null input to prevent application crash
+        If tbBetAmount.Text = "" Or tbBetAmount.Text = Nothing Then
+            tbBetAmount.Text = 0
+        End If
+        ' Verify bet amount is at least $10, less than cash amount, and a multiple of 10, 
+        If tbBetAmount.Text < 10 Then
+            MessageBox.Show("The minimum bet is $10.",
+                            "Invalid bet amount",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            tbBetAmount.Text = 10
+        ElseIf tbBetAmount.Text > intCash Then
+            MessageBox.Show("You can not bet more than your current cash amount.",
+                            "Invalid bet amount",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            tbBetAmount.Text = intCash
+        ElseIf tbBetAmount.Text Mod 10 <> 0 Then
+            MessageBox.Show("The bet amount must be a multiple of $10.",
+                            "Invalid bet amount",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            tbBetAmount.Text = tbBetAmount.Text - (tbBetAmount.Text Mod 10)
+        End If
+        intBetAmount = tbBetAmount.Text
+    End Sub
+    ' Turn on or off roll animations
+    Private Sub cbNoRolls_CheckedChanged(sender As Object, e As EventArgs) Handles cbNoRolls.CheckedChanged
+        If cbNoRolls.CheckState = 1 Then
+            blnRollAnimations = False
+        Else
+            blnRollAnimations = True
+        End If
+    End Sub
+    ' Initiate Timer2 ticks to control dice spinning
+    Private Sub spinDice()
+        Timer2.Interval = 200
+        intTimerTicker = 0
+        Timer2.Start()
+    End Sub
+    ' Display two dice to screen based on the given values
+    Private Sub setDiceImage(img1 As Integer, img2 As Integer)
+        Select Case img1
+            Case 1
+                picBoxDie1.Image = My.Resources.Resource1.dice1
+            Case 2
+                picBoxDie1.Image = My.Resources.Resource1.dice2
+            Case 3
+                picBoxDie1.Image = My.Resources.Resource1.dice3
+            Case 4
+                picBoxDie1.Image = My.Resources.Resource1.dice4
+            Case 5
+                picBoxDie1.Image = My.Resources.Resource1.dice5
+            Case 6
+                picBoxDie1.Image = My.Resources.Resource1.dice6
+            Case Else
+                picBoxDie1.Image = Nothing
+        End Select
+        Select Case img2
+            Case 1
+                picBoxDie2.Image = My.Resources.Resource1.dice1
+            Case 2
+                picBoxDie2.Image = My.Resources.Resource1.dice2
+            Case 3
+                picBoxDie2.Image = My.Resources.Resource1.dice3
+            Case 4
+                picBoxDie2.Image = My.Resources.Resource1.dice4
+            Case 5
+                picBoxDie2.Image = My.Resources.Resource1.dice5
+            Case 6
+                picBoxDie2.Image = My.Resources.Resource1.dice6
+            Case Else
+                picBoxDie2.Image = Nothing
+        End Select
+    End Sub
+    ' Update cash amount for a win or loss and prompt the user to play again or quit
+    Private Sub endRound()
+        ' Update cash
+        If blnWonRound Then
+            intCash += intBetAmount
+        ElseIf blnLostRound = True Then
+            intCash -= intBetAmount
+        End If
+        lblCash.Text = "Cash: $" & intCash
+        ' Check if user wants to continue or quit
+        If MessageBox.Show("Do you wish to play again?",
+                           "Round over",
+                           MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Question) _
+                           = DialogResult.Yes Then
+            intPoint = 0
+            lblPoint.Text = "Point: " & intPoint
+            blnLostRound = False
+            blnWonRound = False
+            blnPointSet = False
+            If intCash < 10 Then
+                endGame()
+            End If
+        Else
+            endGame()
+        End If
+    End Sub
+    ' Display the final cash amount for the user and exit the program
+    Private Sub endGame()
+        If intCash < 10 Then
+            MessageBox.Show("You ran out of cash. Please leave our casino.",
+                           "Game Over",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Thanks for Playing! You cashed out with: $" & intCash,
+                           "Cashed Out",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information)
+        End If
+        Application.Exit()
+    End Sub
+    ' Log the roll result to the tbResults textbox
+    Private Sub logRoll(result As String)
+        ' Print appropriate message to log text box
+        If tbResults.Text <> "" Then
+            tbResults.Text += vbCrLf
+        End If
+        ' Adjust log message based on the provided parameter
+        Select Case result
+            Case "Win"
+                tbResults.Text += "Rolled " & intSum.ToString & vbCrLf &
+                    "--------------------- Round Won! ---------------------"
+            Case "Lose"
+                tbResults.Text += "Rolled " & intSum.ToString & vbCrLf &
+                    "--------------------- Round Lost ---------------------"
+            Case "Point"
+                tbResults.Text += "Rolled " & intSum.ToString & ". Point established."
+            Case "Error"
+                tbResults.Text += "DICE VALUE ERROR (roll ignored)"
+            Case Else
+                tbResults.Text += "Rolled " & intSum.ToString
+        End Select
+        ' Make the TextBox text scroll up
+        Dim ctr As Control = Me.ActiveControl
+        tbResults.Focus()
+        tbResults.AppendText("")
+        tbResults.ScrollToCaret()
+        ctr.Focus()
+    End Sub
+    ' Display final cash amount and exit game, verifying with user first if a point is in play
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        If blnPointSet = True Then
+            If MessageBox.Show("A point is in play, you will lose your bet if you leave!" & vbCrLf & "Do you want to quit?",
+                            "Warning",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Warning) _
+                            = DialogResult.Yes Then
+                intCash -= intBetAmount
+                endGame()
+            End If
+        Else
+            endGame()
+        End If
+    End Sub
+    ' Display a new random dice image on each tick to represent rolling dice
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        intTimerTicker += 1
+        ' Increment tick delay after every 3 ticks
+        If intTimerTicker Mod 3 = 0 Then
+            Timer2.Interval += 200
+        End If
+        ' End rolling animation and stop timer after a given number of ticks
+        If intTimerTicker > 9 Then
+            Timer2.Stop()
+            setDiceImage(intDieOne, intDieTwo)
+        Else
+            Dim tmpDie1 = rollDie()
+            Dim tmpDie2 = rollDie()
+            setDiceImage(tmpDie1, tmpDie2)
+        End If
+    End Sub
+End Class
